b9c7b8e5e5224d6bed05221a79a1f1875d73e9a2
Kirill <astapov@mail.ru>
2024-12-01 18:18:51 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String folderPath = "C:\\Users\\User\\Desktop\\Desktop";
+        File file = new File(folderPath);
+
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles(); //получаем список файлов в папке
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
